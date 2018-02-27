a6a29ec293e7db4b6cefb0aa63211cf4b777d60c
Edward Liang <Edward@Edwards-MacBook-Pro.local>
2018-02-27 11:05:49 -0500

    Added more test data to test

--- test/test_bpm.py
+++ test/test_bpm.py
@@ -10,7 +10,11 @@ log = logging.getLogger(__name__)
 def test_import_data():
     log.debug('Begin bpm detection testing')
     from HeartRateMonitor import HeartRateMonitor
-    hrm = HeartRateMonitor(filename='test_data/test_data1.csv')
-    hrm.detect_bpm()
-    assert hrm.mean_hr_bpm == pytest.approx(75, 10)
+    filenames = ['test_data1.csv', 'test_data10.csv', 'test_data11.csv']
+    # Approximate BPM by manual calculations
+    bpms = [75, 95, 66]
+    for i, name in enumerate(filenames):
+        hrm = HeartRateMonitor(filename='test_data/' + name)
+        hrm.detect_bpm()
+        assert hrm.mean_hr_bpm == pytest.approx(bpms[i], 10)
     log.debug('End bpm detection testing')
