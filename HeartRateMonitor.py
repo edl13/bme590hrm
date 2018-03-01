047a2ce70ec45ffbbb483d9f7eb368eb5e2b6c1f
Edward Liang <ed.l.1324@gmail.com>
2018-03-01 00:28:53 -0500

    Bpm detection (#18)
    * Added rudimentary autocorrelation bpm detection. This closes #6
    
    * Normalized autocorrelation and increased min SNR in peak detection
    
    * Added more test data to test
    
    * Added custom time limits for bpm detection. This closes #13 and closes #14.
    
    * Removed logging_config from requirements
    
    * Removed blank line in __init__.py files to avoid Travis pep8 complaints

--- HeartRateMonitor.py
+++ HeartRateMonitor.py
@@ -5,7 +5,11 @@ and other processing techniques
 
 import numpy as np
 import pandas as pd
-# import json
+from scipy import signal
+import logging
+from . logging_config import config
+logging.basicConfig(**config)
+log = logging.getLogger(__name__)
 
 
 class HeartRateMonitor(object):
@@ -112,3 +116,78 @@ class HeartRateMonitor(object):
         df = pd.read_csv(filename, names=['Time', 'Voltage'])
         data = df.as_matrix()
         self.data = data
+
+    def detect_bpm(self, time=None):
+        '''Detects BPM using autocorrelation.
+        :param time: Time over which to find mean BPM. Defaults to find mean
+        from beginning to end of given signal. If scalar given, mean is found
+        from t = 0 to t = time seconds. If two element list or tuple of times
+        is given, mean is found between the two times. Begin and end sample
+        points chosen to be as close to given arguments as possible.'''
+
+        data = self.data
+        t = data[:, 0]
+        v = data[:, 1]
+        dt = t[1] - t[0]
+        t_lim = None
+
+        if time is None:
+            t_lim = (0, max(t))
+        elif isinstance(time, (list, tuple)):
+            if(len(time) == 2):
+                t_lim = time
+            else:
+                raise ValueError('''Iterable time input must have two elements
+                      for start and end times''')
+                log.error('''Iterable time input must have two elements for start
+                          and end times''')
+        elif isinstance(time, (int, float)):
+            t_lim = (0, time)
+        else:
+            raise TypeError('''Time argument takes scalar or two element
+                  iterable''')
+            log.error('Time argument takes scalar or two element iterable.')
+
+        (start, end) = self.find_nearest_limits(t, t_lim)
+        # Remove dc offsets
+        avg_len = 50
+        v_dc = v - np.convolve(v, np.ones(avg_len) / avg_len, mode='same')
+
+        # Autocorrelation
+        # std = np.std(v_dc)
+        corr1 = np.correlate(v_dc, v_dc, mode='full')
+        corr1 = np.divide(corr1, max(corr1))
+        # corr2 = np.correlate(corr1, corr1, mode='full')
+        corr1 = corr1[int(len(corr1) / 2):]
+
+        # Autocorrelation peak detection with scipy.
+        widths = np.arange(1, 20)
+        peaks = signal.find_peaks_cwt(corr1, widths, noise_perc=70,
+                                      min_snr=45,
+                                      max_distances=np.divide(widths, 2))
+
+        # Record peaks with positive correlation
+        corr_thresh = 0
+        lags = peaks[corr1[peaks] > corr_thresh]
+
+        # Calculate BPM
+        period = lags[1] - lags[0]
+        bpm = 60 / (dt * period)
+        print(bpm)
+        self.mean_hr_bpm = bpm
+        return bpm
+
+    def find_nearest_limits(self, t, t_lim):
+        '''Find nearest t values to given limits
+
+        :param t: Array of sample times
+        :param t_lim: Two element tuple of start and end times
+        :return: Tuple of start and end indices of t
+        '''
+
+        begin = t_lim[0]
+        end = t_lim[1]
+
+        begin_i = np.argmin(np.abs(t - begin))
+        end_i = np.argmin(np.abs(t - end))
+        return (begin_i, end_i)
