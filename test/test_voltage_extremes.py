f4963496a02fec4655100e360cc449abc4fe527e
Edward Liang <Edward@Edwards-MacBook-Pro.local>
2018-03-02 12:10:39 -0500

    Completed unit testing for detect voltage limits. This closes #17, closes #7, and closes #5.

--- test/test_voltage_extremes.py
+++ test/test_voltage_extremes.py
@@ -16,3 +16,8 @@ def test_voltage_extremes():
     with pytest.warns(UserWarning):
         hrm.detect_voltage_extremes(thresh=300, units='mV')
     assert hrm.voltage_extremes == pytest.approx((-680, 1050))
+
+    hrm = HeartRateMonitor(filename='test_data/test_data32.csv', t_units='ms',
+                           v_units='mV')
+    with pytest.warns(UserWarning):
+        hrm.detect_voltage_extremes(thresh=300, units='mV')
