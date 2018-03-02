3a0f5057b9fb2df783ac55c496e527c5d5bc439c
Edward Liang <ed.l.1324@gmail.com>
2018-03-02 00:35:13 -0500

    Added units support and units unit testing (#19)
    * Added units support and units unit testing
    
    * Fixed git commit
    
    * Added matplotlib to requirements
    
    * changed pytest to python -m pytest in travis.yml
    
    * Change matplotlib backend to Agg for travis compatability
    
    * Changed matplotlib backend again for travis
    
    * Added setup.cfg to ignore import formatting error
    
    * Fixed logging_config.py indentation

--- HeartRateMonitor.py
+++ HeartRateMonitor.py
@@ -7,7 +7,14 @@ import numpy as np
 import pandas as pd
 from scipy import signal
 import logging
-from . logging_config import config
+import matplotlib as mpl
+import os
+from logging_config import config
+if os.environ.get('DISPLAY', '') == '':
+        print('no display found. Using non-interactive Agg backend')
+        mpl.use('Agg')
+import matplotlib.pyplot as plt
+
 logging.basicConfig(**config)
 log = logging.getLogger(__name__)
 
@@ -17,14 +24,26 @@ class HeartRateMonitor(object):
     ECG signal
     '''
 
-    def __init__(self, data=None, filename=None):
+    def __init__(self, data=None, filename=None, t_units='ms',
+                 v_units='mV'):
         '''Initialize HeartRateMonitor object
 
         :param data: 2D numpy array with time values in the first column and
         ECG voltage values in the second column. Defaults to None.
         :param filename: CSV file with time in first column and voltage in the
         second column. Defaults to None.
+        :param t_units: Time units, either 'ms', 's', or 'min'. Defaults to
+        's'.
+        :param v_units: Voltage units, either 'mV' or 'V'
         '''
+
+        self.t_units = t_units
+        self.v_units = v_units
+        self.__t_converter = None
+        self.__v_converter = None
+        (self.__t_converter, self.__v_converter) = self.__get_converters(
+            self.t_units, self.v_units)
+
         if data is None and filename is None:
             self.data = []
         elif data is not None:
@@ -34,11 +53,16 @@ class HeartRateMonitor(object):
         else:
             self.data = []
 
+        self.__convert_data()
+
         self.mean_hr_bpm = None
         self.voltage_extremes = None
         self.duration = None
         self.num_beats = None
         self.beats = None
+        self.__filt_data = None
+
+        self.__filter_data()
 
     @property
     def data(self):
@@ -116,8 +140,13 @@ class HeartRateMonitor(object):
         df = pd.read_csv(filename, names=['Time', 'Voltage'])
         data = df.as_matrix()
         self.data = data
+        log.debug(self.data[1, 0])
 
-    def detect_bpm(self, time=None):
+    def __convert_data(self):
+        self.data[:, 0] *= self.__t_converter
+        self.data[:, 1] *= self.__v_converter
+
+    def detect_bpm(self, time=None, units=None):
         '''Detects BPM using autocorrelation.
         :param time: Time over which to find mean BPM. Defaults to find mean
         from beginning to end of given signal. If scalar given, mean is found
@@ -125,16 +154,24 @@ class HeartRateMonitor(object):
         is given, mean is found between the two times. Begin and end sample
         points chosen to be as close to given arguments as possible.'''
 
+        if units is None:
+            units = self.t_units
+
         data = self.data
-        t = data[:, 0]
-        v = data[:, 1]
-        dt = t[1] - t[0]
         t_lim = None
+        (lim_converter, v_con_temp) = self.__get_converters(units,
+                                                            self.v_units)
+        t_raw = data[:, 0]
+        dt = t_raw[1] - t_raw[0]
+
+        log.info('dt found to be {}'.format(dt))
 
         if time is None:
-            t_lim = (0, max(t))
+            t_lim = np.array((0, max(t_raw)))
         elif isinstance(time, (list, tuple)):
             if(len(time) == 2):
+                time = np.array(time)
+                time *= lim_converter
                 t_lim = time
             else:
                 raise ValueError('''Iterable time input must have two elements
@@ -142,40 +179,50 @@ class HeartRateMonitor(object):
                 log.error('''Iterable time input must have two elements for start
                           and end times''')
         elif isinstance(time, (int, float)):
+            time *= lim_converter
             t_lim = (0, time)
         else:
             raise TypeError('''Time argument takes scalar or two element
                   iterable''')
             log.error('Time argument takes scalar or two element iterable.')
 
-        (start, end) = self.find_nearest_limits(t, t_lim)
-        # Remove dc offsets
-        avg_len = 50
-        v_dc = v - np.convolve(v, np.ones(avg_len) / avg_len, mode='same')
+        (start, end) = self.find_nearest_limits(t_raw, t_lim)
+        print(start, end, time)
+        v = self.__filt_data[start:end]
 
+        # Remove dc offsets
+        # avg_len = 50
+        # v_dc = v - np.convolve(
+        #   v, np.ones(avg_len) / avg_len, mode='same')
         # Autocorrelation
         # std = np.std(v_dc)
-        corr1 = np.correlate(v_dc, v_dc, mode='full')
+        corr1 = np.correlate(v, v, mode='full')
         corr1 = np.divide(corr1, max(corr1))
         # corr2 = np.correlate(corr1, corr1, mode='full')
-        corr1 = corr1[int(len(corr1) / 2):]
+        corr1 = corr1[int(len(corr1) / 2) + 000:]
+        # corr1 = np.multiply(corr1, corr1)
 
         # Autocorrelation peak detection with scipy.
-        widths = np.arange(1, 20)
-        peaks = signal.find_peaks_cwt(corr1, widths, noise_perc=70,
-                                      min_snr=45,
-                                      max_distances=np.divide(widths, 2))
-
-        # Record peaks with positive correlation
-        corr_thresh = 0
-        lags = peaks[corr1[peaks] > corr_thresh]
+        widths = np.arange(1, 400)
+        peaks = signal.find_peaks_cwt(corr1, widths, noise_perc=10,
+                                      min_snr=20,
+                                      max_distances=np.divide(widths, 10))
 
         # Calculate BPM
-        period = lags[1] - lags[0]
-        bpm = 60 / (dt * period)
-        print(bpm)
+        try:
+            period = peaks[1] - peaks[0]
+        except IndexError:
+            raise UserWarning('''Only one peak detected in time region specified.
+                              Unable to detect BPM''')
+
+        bpm = 60 * self.__t_converter / (dt * period)
         self.mean_hr_bpm = bpm
-        return bpm
+        plt.plot(corr1)
+        plt.plot(peaks, np.zeros(len(peaks)), 'o')
+        plt.ion()
+        plt.show()
+        log.info('BPM found to be {}'.format(bpm))
+        return v, corr1, peaks, bpm
 
     def find_nearest_limits(self, t, t_lim):
         '''Find nearest t values to given limits
@@ -191,3 +238,51 @@ class HeartRateMonitor(object):
         begin_i = np.argmin(np.abs(t - begin))
         end_i = np.argmin(np.abs(t - end))
         return (begin_i, end_i)
+
+    def __get_converters(self, t_units, v_units):
+
+        if type(t_units) is not str:
+            raise TypeError('Please input string for time units')
+
+        if type(v_units) is not str:
+            raise TypeError('Please input string for voltage units')
+
+        if(t_units == 's'):
+            t_converter = 1000
+        elif(t_units == 'ms'):
+            t_converter = 1
+        elif(t_units == 'min'):
+            t_converter = 60000
+        else:
+            raise ValueError('Time units must be \'s\', \'ms\', or \'min\'.')
+
+        if(v_units == 'V'):
+            v_converter = 1000
+        elif(v_units == 'mV'):
+            v_converter = 1
+        else:
+            raise ValueError('Voltage units must be \'mV\' or \'V\'.')
+
+        return (t_converter, v_converter)
+
+    def __filter_data(self):
+        '''Filter raw data with 5-15 Hz passband according to Pan-Tompkins
+        algorithm'''
+
+        dt = self.data[1, 0] - self.data[0, 0]  # dt in ms
+        nyq = (1 / (dt / 1000)) * 0.5
+
+        low = 5 / nyq
+        hi = 15 / nyq
+
+        b, a = signal.butter(2, (low, hi), btype='bandpass')
+        filt = signal.lfilter(b, a, self.data[:, 1])
+
+        # Rectify
+        filt[filt < 0] = 0
+
+        # Square
+        filt = np.multiply(filt, filt)
+
+        self.__filt_data = filt
+        plt.ion()
--- test/test_bpm.py
+++ test/test_bpm.py
@@ -8,25 +8,28 @@ logging.basicConfig(**config)
 log = logging.getLogger(__name__)
 
 
-def test_import_data():
+def test_bpm():
     log.debug('Begin bpm detection testing')
     from ..HeartRateMonitor import HeartRateMonitor
     filenames = ['test_data1.csv', 'test_data10.csv', 'test_data11.csv']
     # Approximate BPM by manual calculations
-    bpms = [75, 95, 66]
+    bpms = [74, 93, 66]
     for i, name in enumerate(filenames):
-        hrm = HeartRateMonitor(filename='test_data/' + name)
+        hrm = HeartRateMonitor(filename='test_data/' + name, t_units='s',
+                               v_units='V')
         hrm.detect_bpm()
-        assert hrm.mean_hr_bpm == pytest.approx(bpms[i], 10)
+        assert hrm.mean_hr_bpm == pytest.approx(bpms[i], 0.2)
     log.debug('End bpm detection testing')
 
     for i, name in enumerate(filenames):
-        hrm = HeartRateMonitor(filename='test_data/' + name)
-        hrm.detect_bpm(time=(5, 10))
-        assert hrm.mean_hr_bpm == pytest.approx(bpms[i], 10)
+        hrm = HeartRateMonitor(filename='test_data/' + name, t_units='s',
+                               v_units='V')
+        hrm.detect_bpm(time=(5, 300), units='s')
+        assert hrm.mean_hr_bpm == pytest.approx(bpms[i], 0.2)
 
     # Test time input
-    hrm = HeartRateMonitor(filename='test_data/test_data1.csv')
+    hrm = HeartRateMonitor(filename='test_data/test_data1.csv', t_units='s',
+                           v_units='V')
     with pytest.raises(ValueError):
         hrm.detect_bpm(time=(1, 2, 3))
     with pytest.raises(TypeError):
